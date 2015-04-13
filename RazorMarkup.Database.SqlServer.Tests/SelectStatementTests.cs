1bc25e2c0e692bfb6ed13e8b5ddeff41d820ae77
jeremybeavon <jeremybeavon@users.noreply.github.com>
2015-04-13 22:30:40 +1200

    Add more SELECT unit tests

--- RazorMarkup.Database.SqlServer.Tests/SelectStatementTests.cs
+++ RazorMarkup.Database.SqlServer.Tests/SelectStatementTests.cs
@@ -59,6 +59,19 @@ FROM table1";
         }
 
         [TestMethod]
+        public void Test_SelectWithInto_GeneratesCorrectTextFromRazorPage()
+        {
+            const string expectedSql = @"SELECT *
+INTO tempTable
+FROM table1";
+            Sql.Query().Select().AllColumns()
+                .Into(new TableName("tempTable"))
+                .From().Table(new TableName("table1"))
+                .End().Query()
+                .ToSqlStringViaRazorPageIs(expectedSql);
+        }
+
+        [TestMethod]
         public void Test_SelectWithInnerJoin_GeneratesCorrectTextFromRazorPage()
         {
             const string expectedSql = @"SELECT *
@@ -69,6 +82,134 @@ ON table1.column1 = table2.column1";
                 .Select().AllColumns()
                 .From().Table(new TableName("table1"))
                 .InnerJoin().Table(new TableName("table2"))
+                .On(() => new ColumnName("table1.column1") == new ColumnName("table2.column1"))
+                .End().Query()
+                .ToSqlStringViaRazorPageIs(expectedSql);
+        }
+
+        [TestMethod]
+        public void Test_SelectWithNestedInnerJoins_GeneratesCorrectTextFromRazorPage()
+        {
+            const string expectedSql = @"SELECT *
+FROM table1
+INNER JOIN table2
+INNER JOIN table3
+ON table2.column1 = table3.column1
+ON table1.column1 = table2.column1";
+            Sql.Query()
+                .Select().AllColumns()
+                .From().Table(new TableName("table1"))
+                .InnerJoin().Table(new TableName("table2"))
+                .InnerJoin().Table(new TableName("table3"))
+                .On(() => new ColumnName("table2.column1") == new ColumnName("table3.column1"))
+                .On(() => new ColumnName("table1.column1") == new ColumnName("table2.column1"))
+                .End().Query()
+                .ToSqlStringViaRazorPageIs(expectedSql);
+        }
+
+        [TestMethod]
+        public void Test_SelectWithLeftJoin_GeneratesCorrectTextFromRazorPage()
+        {
+            const string expectedSql = @"SELECT *
+FROM table1
+LEFT JOIN table2
+ON table1.column1 = table2.column1";
+            Sql.Query()
+                .Select().AllColumns()
+                .From().Table(new TableName("table1"))
+                .LeftJoin().Table(new TableName("table2"))
+                .On(() => new ColumnName("table1.column1") == new ColumnName("table2.column1"))
+                .End().Query()
+                .ToSqlStringViaRazorPageIs(expectedSql);
+        }
+
+        [TestMethod]
+        public void Test_SelectWithNestedLeftJoins_GeneratesCorrectTextFromRazorPage()
+        {
+            const string expectedSql = @"SELECT *
+FROM table1
+LEFT JOIN table2
+LEFT JOIN table3
+ON table2.column1 = table3.column1
+ON table1.column1 = table2.column1";
+            Sql.Query()
+                .Select().AllColumns()
+                .From().Table(new TableName("table1"))
+                .LeftJoin().Table(new TableName("table2"))
+                .LeftJoin().Table(new TableName("table3"))
+                .On(() => new ColumnName("table2.column1") == new ColumnName("table3.column1"))
+                .On(() => new ColumnName("table1.column1") == new ColumnName("table2.column1"))
+                .End().Query()
+                .ToSqlStringViaRazorPageIs(expectedSql);
+        }
+
+        [TestMethod]
+        public void Test_SelectWithRightJoin_GeneratesCorrectTextFromRazorPage()
+        {
+            const string expectedSql = @"SELECT *
+FROM table1
+RIGHT JOIN table2
+ON table1.column1 = table2.column1";
+            Sql.Query()
+                .Select().AllColumns()
+                .From().Table(new TableName("table1"))
+                .RightJoin().Table(new TableName("table2"))
+                .On(() => new ColumnName("table1.column1") == new ColumnName("table2.column1"))
+                .End().Query()
+                .ToSqlStringViaRazorPageIs(expectedSql);
+        }
+
+        [TestMethod]
+        public void Test_SelectWithNestedRightJoins_GeneratesCorrectTextFromRazorPage()
+        {
+            const string expectedSql = @"SELECT *
+FROM table1
+RIGHT JOIN table2
+RIGHT JOIN table3
+ON table2.column1 = table3.column1
+ON table1.column1 = table2.column1";
+            Sql.Query()
+                .Select().AllColumns()
+                .From().Table(new TableName("table1"))
+                .RightJoin().Table(new TableName("table2"))
+                .RightJoin().Table(new TableName("table3"))
+                .On(() => new ColumnName("table2.column1") == new ColumnName("table3.column1"))
+                .On(() => new ColumnName("table1.column1") == new ColumnName("table2.column1"))
+                .End().Query()
+                .ToSqlStringViaRazorPageIs(expectedSql);
+        }
+
+        [TestMethod]
+        public void Test_SelectWithFullJoin_GeneratesCorrectTextFromRazorPage()
+        {
+            const string expectedSql = @"SELECT *
+FROM table1
+FULL JOIN table2
+ON table1.column1 = table2.column1";
+            Sql.Query()
+                .Select().AllColumns()
+                .From().Table(new TableName("table1"))
+                .FullJoin().Table(new TableName("table2"))
+                .On(() => new ColumnName("table1.column1") == new ColumnName("table2.column1"))
+                .End().Query()
+                .ToSqlStringViaRazorPageIs(expectedSql);
+        }
+
+        [TestMethod]
+        public void Test_SelectWithNestedFullJoins_GeneratesCorrectTextFromRazorPage()
+        {
+            const string expectedSql = @"SELECT *
+FROM table1
+FULL JOIN table2
+FULL JOIN table3
+ON table2.column1 = table3.column1
+ON table1.column1 = table2.column1";
+            Sql.Query()
+                .Select().AllColumns()
+                .From().Table(new TableName("table1"))
+                .FullJoin().Table(new TableName("table2"))
+                .FullJoin().Table(new TableName("table3"))
+                .On(() => new ColumnName("table2.column1") == new ColumnName("table3.column1"))
                 .On(() => new ColumnName("table1.column1") == new ColumnName("table2.column1"))
                 .End().Query()
                 .ToSqlStringViaRazorPageIs(expectedSql);
